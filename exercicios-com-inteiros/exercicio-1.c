c5aea2ab61d8c986f294dc6323c9bac173ca88d8
Mario Netto <marionetto@MacBook-de-Mario.local>
2014-08-09 11:58:40 -0300

    Exercício novo dec -> bin

--- exercicios-com-inteiros/exercicio-1.c
+++ exercicios-com-inteiros/exercicio-1.c
@@ -1,3 +1,7 @@
+/* 
+1.  Dada uma seqüência de números inteiros não-nulos, seguida por 0, imprimir seus quadrados. 
+*/
+
 #include <stdio.h>
 
 int main (){
--- exercicios-com-inteiros/exercicio-2.c
+++ exercicios-com-inteiros/exercicio-2.c
@@ -1,7 +1,12 @@
+/*
+2. Dado um número natural na base decimal, transformá-lo para a base binária.
+Exemplo: Dado 18 a saída deverá ser 10010. 
+*/
+
 #include <stdio.h>
 
 int main(){
-
+	
 	int num, pot, aux, bin, mod;
 
 	printf ("Informe valor decimal: ");
